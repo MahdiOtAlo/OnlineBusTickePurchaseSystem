f5c5ed1c8ce9bdde4a91e290c483b18db75ca31e
OtAlo <mahdiotalo@gmail.com>
2025-12-01 00:16:59 +0330

    building project only

---
+++ src/main/java/ir/maktabsharif/onlinebustickepurchasesystem/OnlineBusTickePurchaseSystemApplication.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.onlinebustickepurchasesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBusTickePurchaseSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBusTickePurchaseSystemApplication.class, args);
+    }
+
+}
